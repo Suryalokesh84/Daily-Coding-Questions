0bb324fadd054d28acce6a92d58680ce07d689ef
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-18 22:54:19 +0530

    day4  streak

--- Day4.py
+++ Day4.py
@@ -184,3 +184,59 @@ for _ in range(T):
 # 2
 # 60
 # 35
+
+
+
+
+
+
+# 📝 Question:
+#leet code problem
+# 415. Add Strings
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+ 
+
+# Example 1:
+
+# Input: num1 = "11", num2 = "123"
+# Output: "134"
+# Example 2:
+
+# Input: num1 = "456", num2 = "77"
+# Output: "533"
+# Example 3:
+
+# Input: num1 = "0", num2 = "0"
+# Output: "0"
+ 
+
+# Constraints:
+
+# 1 <= num1.length, num2.length <= 104
+# num1 and num2 consist of only digits.
+# num1 and num2 don't have any leading zeros except for the zero itself.
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        i, j = len(num1) - 1, len(num2) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            digit1 = int(num1[i]) if i >= 0 else 0
+            digit2 = int(num2[j]) if j >= 0 else 0
+
+            total = digit1 + digit2 + carry
+            result.append(str(total % 10))
+            carry = total // 10
+
+            i -= 1
+            j -= 1
+
+        return "".join(reversed(result))
