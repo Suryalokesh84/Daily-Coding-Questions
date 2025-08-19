eb14590c9e2ab0d5c35e321434c616828e744c67
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-19 22:51:13 +0530

    day5  streak

--- Day5.py
+++ Day5.py
@@ -246,3 +246,41 @@ for _ in range(t):
         print("YES")
     else:
         print("NO")
+
+
+# Example usage:# n = 3, x = 3
+# Output: YES (valid game)
+
+
+
+
+
+
+# LEET CODE
+# 258. Add Digits
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+# Example 1:
+
+# Input: num = 38
+# Output: 2
+# Explanation: The process is
+# 38 --> 3 + 8 --> 11
+# 11 --> 1 + 1 --> 2 
+# Since 2 has only one digit, return it.
+# Example 2:
+
+# Input: num = 0
+# Output: 0
+ 
+
+# Constraints:
+
+# 0 <= num <= 231 - 1
