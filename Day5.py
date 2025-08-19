467b67a1595520bab08109622545b761ef55a851
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-19 22:52:08 +0530

    day5  streak

--- Day5.py
+++ Day5.py
@@ -284,3 +284,9 @@ for _ in range(t):
 # Constraints:
 
 # 0 <= num <= 231 - 1
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            num = sum(int(d) for d in str(num))
+        return num
