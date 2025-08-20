c052794d23238e437d7c12c3ae855d6b5118e63f
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-20 12:21:12 +0530

    day6  streak

--- Day6.py
+++ Day6.py
@@ -113,15 +113,14 @@ class Solution:
     # Q7. Prime Number with Comments
   
 def is_prime_verbose(n):
-    # Prime numbers are greater than 1
     if n <= 1:
         return False
     
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            return False  # Found a divisor, not prime
+            return False  
     
-    return True  # No divisors found, number is prime
+    return True  
 
 print(is_prime_verbose(29))
 # Example usage:
