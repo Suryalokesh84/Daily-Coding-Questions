1427b335d73033cc6f472fe41c18d4a85cfb0297
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-20 12:20:51 +0530

    day6  streak

--- Day6.py
+++ Day6.py
@@ -117,7 +117,6 @@ def is_prime_verbose(n):
     if n <= 1:
         return False
     
-    # Check divisibility up to square root of n
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False  # Found a divisor, not prime
