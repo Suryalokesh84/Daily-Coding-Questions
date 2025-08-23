97be0efc4e4d92068deced4812912753675a419b
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-23 10:45:15 +0530

    day9  streak

--- Day9.py
+++ Day9.py
@@ -98,3 +98,18 @@ t=int(input())
 for i in range(t):
     a,b=map(int,input().split())
     print(max(a-b,b-a))
+
+
+# Leet Code 
+# 3197. Find the Minimum Area to Cover All Ones II
+# Solved
+# Hard
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You are given a 2D binary array grid. You need to find 3 non-overlapping rectangles having non-zero areas with horizontal and vertical sides such that all the 1's in grid lie inside these rectangles.
+
+# Return the minimum possible sum of the area of these rectangles.
+
+# Note that the rectangles are allowed to touch.
