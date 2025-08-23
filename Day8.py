6ec2a3856278f49ff13d1540998b4533f896d24c
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-23 10:07:51 +0530

    day8  streak

--- Day8.py
+++ Day8.py
@@ -40,10 +40,10 @@ class Solution:
 
     # Q4. Fibonacci Series (first n terms)
     def fibonacci(n):
-    seq=[0,1]
-    for i in range(2,n):
-        seq.append(seq[i-1]+seq[i-2])
-    return seq[:n]
+        seq=[0,1]
+        for i in range(2,n):
+            seq.append(seq[i-1]+seq[i-2])
+        return seq[:n]
 
 print(fibonacci(10))
 # Example usage:
