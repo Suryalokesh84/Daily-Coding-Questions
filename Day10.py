4218ab1a2b3564803a174c7c9f7cfa77ffea307a
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-24 22:07:18 +0530

    day10 streak

--- Day10.py
+++ Day10.py
@@ -127,3 +127,18 @@ for _ in range(t):
 
 # 1 <= nums.length <= 105
 # nums[i] is either 0 or 1.
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        left = 0
+        zeros = 0
+        res = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros += 1
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+            res = max(res, right - left)
+        return res
