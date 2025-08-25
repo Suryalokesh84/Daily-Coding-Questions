11376dc465790c10e7ee33a3e917edfa7498159f
suryalokesh84 <suryalokeshh@gmail.com>
2025-08-25 11:12:12 +0530

    day11 streak

--- Day11.py
+++ Day11.py
@@ -138,3 +138,29 @@ for _ in range(t):
 # 1 <= m, n <= 104
 # 1 <= m * n <= 104
 # -105 <= mat[i][j] <= 105
+
+
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        if not mat or not mat[0]:
+            return []
+        
+        m, n = len(mat), len(mat[0])
+        result = []
+        
+        for d in range(m + n - 1):
+            temp = []
+            r = 0 if d < n else d - n + 1
+            c = d - r
+            
+            while r < m and c >= 0:
+                temp.append(mat[r][c])
+                r += 1
+                c -= 1
+            
+            if d % 2 == 0:
+                result.extend(temp[::-1])
+            else:
+                result.extend(temp)
+        
+        return result
