bfe3c51cb5111fe745695db40544bb2c2ccff22f
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-02 11:15:12 +0530

    day18 streak

--- Day18.py
+++ Day18.py
@@ -216,3 +216,32 @@ class Solution:
 # Example usage:
 # points = [[6,2],[4,4],[2,6]]
 
+
+
+# # ======================================================================
+# Setting Up a MERN Stack Project
+# To setup MERN stack we need to create a folder structure for both frontend and backend. Then we have to define database schema to store and retrieve data from the database.
+
+# Follow the below steps to create a basic structure
+
+# Step 1: After the code editor is installed, create a new project folder. Then go to the project folder in command prompt/terminal and type below commands to create folder for frontend and backend 
+
+# mkdir frontend
+# mkdir backend
+# Step 2: Navigate to the frontend folder using the command
+
+# cd frontend
+# Step 3: Initialize a React Project using the command
+
+# npx create-react-app
+# Step 4: Now navigate to the backend folder using the command
+
+# cd..
+# cd backend
+# Step 5: Initialize the project backend using the command
+
+# npm init -y
+# Step 6: Install Express and other backend dependencies using the command
+
+# npm i mongodb express cors dotenv 
+# After following all the above steps a basic structure for MERN Stack application is created.
