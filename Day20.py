8888efed41acbbb9fc76b0897a4e2e06412216e1
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-04 11:29:24 +0530

    day20 streak

--- Day20.py
+++ Day20.py
@@ -8,14 +8,17 @@
 # The first line of the input contains an integer T, the number of test cases. T lines follow. Each line contains an integer N whose square root needs to be computed.
 
 # Output
+
 # For each line of the input, output the square root of the input integer, rounded down to the nearest integer, in a new line.
 
 # Constraints
+
 # 1<=T<=20
 # 1<=N<=10000
 
 # Sample 1:
 # Input
+
 # Output
 # 3
 # 10
