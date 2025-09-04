154424f927ff1e06c996fb1e012741a956f39576
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-04 11:32:07 +0530

    day20 streak

--- Day20.py
+++ Day20.py
@@ -46,3 +46,6 @@ for _ in range(t):
 # 2
 # 100
 # Explanation:
+# The square root of 10 is approximately 3.16, which rounds down to 3.
+# The square root of 5 is approximately 2.23, which rounds down to 2.
+# The square root of 10000 is exactly 100.  
