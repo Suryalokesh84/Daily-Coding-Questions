e3ff8d9fd3f3082e1daede9c8f314d0d0846ae5b
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-04 11:36:29 +0530

    day20 streak

--- Day20.py
+++ Day20.py
@@ -117,3 +117,12 @@ for _ in range(t):
 # Constraints:
 
 # 1 <= x, y, z <= 100
+
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        d1, d2 = abs(x - z), abs(y - z)
+        if d1 < d2:
+            return 1
+        elif d2 < d1:
+            return 2
+        return 0
