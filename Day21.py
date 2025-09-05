7d064c30d0139d637f7ec426a00db4ff94bf2d48
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-05 19:21:26 +0530

    day21 streak

--- Day21.py
+++ Day21.py
@@ -163,3 +163,12 @@ for i in range(int(input())):
 
 # 1 <= num1 <= 109
 # -109 <= num2 <= 109
+
+
+class Solution:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        for k in range(1, 61):
+            val = num1 - k * num2
+            if val >= k and bin(val).count("1") <= k:
+                return k
+        return -1
