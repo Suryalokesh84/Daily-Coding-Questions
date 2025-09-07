7bc35521af4447da94f9caa521bf919d86ed1a29
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-07 21:36:11 +0530

    day23 streak

--- Day23.py
+++ Day23.py
@@ -119,3 +119,12 @@ for _ in range(t):
 # ========================================================
 # 2nd Question
 # leetcode problem
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+        for i in range(1, n // 2 + 1):
+            res.append(i)
+            res.append(-i)
+        if n % 2 == 1:
+            res.append(0)
+        return res
