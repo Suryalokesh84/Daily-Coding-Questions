503f326bd62cb525cc66fddd95543af3e44caa03
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-13 11:58:48 +0530

    day29 streak

--- Day29.py
+++ Day29.py
@@ -161,3 +161,14 @@ for i in range(t):
 
 # 1 <= s.length <= 100
 # s consists of lowercase English letters only.
+
+
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        vowels = set("aeiou")
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+        max_vowel = max([freq[ch] for ch in freq if ch in vowels], default=0)
+        max_consonant = max([freq[ch] for ch in freq if ch not in vowels], default=0)
+        return max_vowel + max_consonant
