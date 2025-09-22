ab996d06fe8d2dd93f62408c1dceee20709474d7
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-22 22:34:04 +0530

    day37 streak

--- Day37.py
+++ Day37.py
@@ -261,3 +261,18 @@ for _ in range(T):
 
 # 1 <= nums.length <= 100
 # 1 <= nums[i] <= 100
+
+
+
+
+
+
+
+
+from collections import Counter
+
+class Solution:
+    def maxFrequencyElements(self, nums):
+        freq = Counter(nums)
+        max_freq = max(freq.values())
+        return sum(count for count in freq.values() if count == max_freq)
