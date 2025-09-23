b92d26ffee69cf889be6e008ae2b3377cffe5156
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-23 11:20:09 +0530

    day38 streak

---
+++ Day38.py
@@ -0,0 +1,45 @@
+# # Minimum Spanning Tree [Graphs]
+# You are given a connected, undirected, weighted graph with N vertices and M edges. Your task is to find the weight of the Minimum Spanning Tree (MST) using either Kruskal’s Algorithm or Prim’s Algorithm. The MST of a graph is a subset of edges that connects all vertices together without cycles and with the minimum possible total edge weight.
+
+# Input Format
+
+# First line: Two integers N and M → number of vertices and edges. Next M lines: Three integers u v w, representing an undirected edge between u and v with weight w.
+
+# Constraints
+
+# 3 <= N <= 20
+
+# Output Format
+
+# Print the total weight of the MST.
+
+# Sample Input 0
+
+# 4 5
+# 0 1 10
+# 0 2 6
+# 0 3 5
+# 1 3 15
+# 2 3 4
+# Sample Output 0
+
+# MST weight = 19
+# Explanation 0
+
+# minimum spanning tree with the below edges
+
+# 0->1 = 10 0->3 = 5 2->3 = 4
+
+# 10+5+4 = 19
+
+# Contest ends in 13 hours
+# Submissions: 284
+# Max Score: 10
+# Difficulty: Medium
+# Rate This Challenge:
+
+    
+# More
+ 
+# 1
+# ​
