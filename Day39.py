92af90b45b80a65413c9d5a2b5de03b57f1fc586
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-24 14:05:12 +0530

    day39 streak

---
+++ Day39.py
@@ -0,0 +1,38 @@
+# # code chef problem
+# Decrement OR Increment
+# Write a program to obtain a number 
+# N
+# N and increment its value by 1 if the number is divisible by 4 
+# o
+# t
+# h
+# e
+# r
+# w
+# i
+# s
+# e
+# otherwise decrement its value by 1.
+
+# Input Format
+# First line will contain a number 
+# N
+# N.
+
+# Output Format
+# Output a single line, the new value of the number.
+
+# Constraints
+# 0
+# ≤
+# N
+# ≤
+# 1000
+# 0≤N≤1000
+# Sample 1:
+# Input
+# Output
+# 5
+# 4
+# Explanation:
+# Since 5 is not divisible by 4 hence, its value is decreased by 1.
