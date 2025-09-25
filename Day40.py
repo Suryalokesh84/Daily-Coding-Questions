5b710e18e4ea427626d060b9e550a7927cb29aaa
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-25 13:11:12 +0530

    day40 streak

--- Day40.py
+++ Day40.py
@@ -384,3 +384,13 @@ for _ in range(T):
 # ==============================================================================================
 
 # leetcode problem
+
+
+
+class Solution:
+    def minimumTotal(self, triangle):
+        dp = triangle[-1][:]
+        for row in range(len(triangle) - 2, -1, -1):
+            for i in range(len(triangle[row])):
+                dp[i] = triangle[row][i] + min(dp[i], dp[i + 1])
+        return dp[0]
