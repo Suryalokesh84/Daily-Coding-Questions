b32bf976c6d531f6c2943a0215cceb5dbef7ea07
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-27 18:08:56 +0530

    day41 streak

--- Day41.py
+++ Day41.py
@@ -41,3 +41,32 @@ for _ in range(T):
 
 # ============================================================================================================
     # leetcode problem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from typing import List
+from itertools import combinations
+
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        def area(a, b, c):
+            return abs(a[0]*(b[1]-c[1]) + b[0]*(c[1]-a[1]) + c[0]*(a[1]-b[1])) / 2
+        return max(area(a, b, c) for a, b, c in combinations(points, 3))
