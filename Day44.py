2890b20b8f3b194f81a82a2dadbe0087385707a5
suryalokesh84 <suryalokeshh@gmail.com>
2025-09-30 10:49:22 +0530

    day44 streak

--- Day44.py
+++ Day44.py
@@ -131,3 +131,16 @@ for _ in range(t):
 
 # 1 <= nums.length <= 1000
 # 0 <= nums[i] <= 9
+
+
+
+
+
+
+
+
+class Solution:
+    def triangularSum(self, nums):
+        while len(nums) > 1:
+            nums = [(nums[i] + nums[i + 1]) % 10 for i in range(len(nums) - 1)]
+        return nums[0]
