b85ec34562639ee6e73574a4db8bc4c5d52c1c64
suryalokesh84 <suryalokeshh@gmail.com>
2025-10-01 12:01:41 +0530

    day4 streak

--- Day45.py
+++ Day45.py
@@ -1,1 +1,20 @@
 # leet code problem
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        empty = numBottles
+        while empty >= numExchange:
+            new_bottles = empty // numExchange
+            total += new_bottles
+            empty = empty % numExchange + new_bottles
+        return total
