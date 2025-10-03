e7b30236d55a4e136567b7b612666aa75b98583d
suryalokesh84 <suryalokeshh@gmail.com>
2025-10-03 22:14:44 +0530

    day47 streak

--- Day47.py
+++ Day47.py
@@ -1,1 +1,22 @@
-# recursion notes
+# # recursion notes
+
+
+
+# 🌀 Recursion in Python – Basic Notes
+# 🔹 What is Recursion?
+
+# Recursion is a programming technique where a function calls itself to solve a smaller version of a problem.
+
+# 🔹 Structure of a Recursive Function
+
+# A recursive function must have:
+
+# Base case – the condition that stops recursion.
+
+# Recursive case – the part where the function calls itself.
+
+# def recursive_function():
+#     if base_condition:
+#         return result
+#     else:
+#         return recursive_function()
